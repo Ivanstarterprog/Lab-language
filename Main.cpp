832dc737ab966da32c5c488958766cbdd970399c
Ivanstarterprog <61678310+Ivanstarterprog@users.noreply.github.com>
2023-09-26 15:46:39 +0700

    Попытался исправить ошибки первой версии программы

--- Main.cpp
+++ Main.cpp
@@ -1,43 +1,41 @@
-/*************************
-* Автор: Нургазин И.В.   *
-* Дата: 20.09.2023       *
-* Вариант: 6             *
-*************************/
+/**********************************
+* Автор: Нургазин И.В.            *
+* Дата: 20.09.2023                *
+* Вариант: 6                      *
+* https://onlinegdb.com/U3SNoYzSV *
+**********************************/
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int main()
-{
+int main(){
     const double PI = 3.14159;
-    double a, b, c;//Стороны треугольника
-    double A, B, C;//Углы треугольника
-    double p;//Полупериметр треугольника
+    double a, b, c; //Стороны треугольника
+    double A, B, C, checkC; //Углы треугольника
+    double p; //Полупериметр треугольника
+    double paramA, paramB, paramC;
 
-    cout << "Введите сторону a: ";
-    cin >> a;
-    cout << "Введите сторону b: ";
-    cin >> b;
-    cout << "Введите сторону c: ";
-    cin >> c;
-
-    p = (a + b + c)/2;
-
-    double paramA = (b*b + c*c - a*a)/(2*b*c);
+    cout << "Введите сторону a: ";  cin >> a;
+    cout << "Введите сторону b: ";  cin >> b;
+    cout << "Введите сторону c: ";  cin >> c;
+ 
+    paramA = (b*b + c*c - a*a) / (2*b*c);
     A = acos(paramA);
-    cout << "Угол A = " << A << endl;
     
-    double paramB = (b*sin(A))/a;
+    paramB = (b*sin(A)) / a;
     B = asin(paramB);
-    cout << "Угол B = " << B << endl;
     
     C = PI - A - B;
-    cout << "Угол C = " << C << endl;
     
-    double paramC = +((p - a)*(p - b)) / (p*(p - c));
-    C = 2 * atan(sqrt(paramC));
-    cout << "Угол С, согласно второй формуле = " << C << endl;
+    p = (a + b + c) / 2;
+    paramC = ((p - a)*(p - b)) / (p*(p - c));
+    checkC = 2 * atan(sqrt(paramC));
+    
+    cout << "Угол A = " << A << endl << 
+    "Угол B = " << B << endl << 
+    "Угол C = " << C << endl << 
+    "Угол С, согласно второй формуле = " << checkC << endl;
     
     return 0;
 }
